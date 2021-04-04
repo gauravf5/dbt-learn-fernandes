687e6cdea262ba13b81fe23587d953149e5aac86
Gaurav Fernandes <gaurav.fernandes@gmail.com>
2021-04-04 02:43:46 +0000

    cleanedup jaffle_shop project

--- models/staging/stg_payments.sql
+++ models/staging/stg_payments.sql
@@ -1,7 +1,9 @@
 select 
-        id as payment_id,
-        orderid as order_id,
-        status,
-        amount
+    
+    id as payment_id,
+    orderid as order_id,
+    paymentmethod as payment_method,
+    status,
+    amount/100 as amount
 
-    from  RAW.STRIPE.PAYMENT
+from {{source ('stripe','payment')}}
